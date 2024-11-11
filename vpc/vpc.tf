8989a69c8072a779e5c00d17b5d38b2b116b34d8
BABIK Guillaume <guillaumebabik@users.noreply.github.com>
2024-11-11 18:34:58 +0100

    Update vpc.tf

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,1 +1,34 @@
+# vpc.tf
+provider "aws" {
+  region = "us-west-2"
+}
 
+data "aws_availability_zones" "available" {}
+
+resource "aws_vpc" "eks_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "eks_subnets" {
+  count                   = 2
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.${count.index}.0/24"
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "eks_igw" {
+  vpc_id = aws_vpc.eks_vpc.id
+}
+
+resource "aws_route_table" "eks_route_table" {
+  vpc_id = aws_vpc.eks_vpc.id
+}
+
+resource "aws_route" "eks_route" {
+  route_table_id         = aws_route_table.eks_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.eks_igw.id
+}
